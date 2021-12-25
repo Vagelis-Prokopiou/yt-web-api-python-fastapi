83c265882d66a645482eb779e22851dd85ae48de
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-25 10:23:01 +0200

    Done

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Run: uvicorn app:app --reload
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,26 +10,26 @@ async def root():
     return {"message": "Hello World"}
 
 
-# def getUsers():
-#     users = []
-#     for index in range(1, 1001):
-#         strIndex = str(index)
-#         firstName = "FirstName" + strIndex
-#         lastName = "LastName" + strIndex
-#         framework = "Python (Flask)"
-#         users.append({
-#             "index": index,
-#             "FirstName": firstName,
-#             "LastName": lastName,
-#             "age": 25,
-#             "framework": framework,
-#         })
-#     return users
-#
-#
-# @app.route("/users")
-# def users():
-#     return jsonify(getUsers())
+def getUsers():
+    users = []
+    for index in range(1, 1001):
+        strIndex = str(index)
+        firstName = "FirstName" + strIndex
+        lastName = "LastName" + strIndex
+        framework = "Python (FastAPI)"
+        users.append({
+            "index": index,
+            "FirstName": firstName,
+            "LastName": lastName,
+            "age": 25,
+            "framework": framework,
+        })
+    return users
+
+
+@app.get("/users")
+def users():
+    return getUsers()
 
 
 if __name__ == "__main__":
